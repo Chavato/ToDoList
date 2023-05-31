7fd75b9642d5d36f78ceb333b48eb54659157d89
Rafael Santos <62575388+Chavato@users.noreply.github.com>
2023-05-31 00:14:10 -0300

    initial commit

---
+++ src/Example.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoList.Example
+{
+    public class NewClass
+    {
+        public bool NewMethod(bool modifier)
+        {
+            return modifier;
+        }
+    }
+}
