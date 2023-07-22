f1b0b194c5ea87f846911828c3d56c6c5a0db827
Rafael Santos <62575388+Chavato@users.noreply.github.com>
2023-07-22 15:53:49 -0300

    refactor: change architecture to Clean Architecture and create tests project

--- src/ToDoList.WebApi/Program.cs
+++ src/ToDoList.WebApi/Program.cs
@@ -13,11 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = "";
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ tests/ToDoList.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ToDoList.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
