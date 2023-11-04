6b716bcc57750d3eeae72e231bd4a39fa2e7a418
Rafael Santos <chavesrafael9@gmail.com>
2023-11-04 13:18:00 -0300

    feat: create repositories interface in domain layer

---
+++ src/ToDoList.Domain/Interfaces/RepositoriesInterfaces/ICheckListRepository.cs
@@ -0,0 +1,9 @@
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Domain.Interfaces.RepositoriesInterfaces
+{
+    public interface ICheckListRepository : IRepository<CheckList>
+    {
+        
+    }
+}
---
+++ src/ToDoList.Domain/Interfaces/RepositoriesInterfaces/IRepository.cs
@@ -0,0 +1,18 @@
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Domain.Interfaces.RepositoriesInterfaces
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        Task<TEntity> GetAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task CreateRangeAsync(IEnumerable<TEntity> entities);
+        Task UpdateAsync(TEntity entity);
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities);
+        Task DeleteAsync(TEntity entity);
+        Task DeleteRangeAsync(IEnumerable<TEntity> entities);
+        Task<bool> HasAnyData();
+        Task<int> SaveChangesAsync();
+    }
+}
