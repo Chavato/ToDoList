61e41e6a036603af0851ee5801c0f10cca428759
Rafael Santos <chavesrafael9@gmail.com>
2023-11-04 13:18:32 -0300

    feat: create exception classes

---
+++ src/ToDoList.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,20 @@
+namespace ToDoList.Domain.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+        {
+
+        }
+
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+
+        public BadRequestException(string message, Exception inner) : base()
+        {
+
+        }
+    }
+}
