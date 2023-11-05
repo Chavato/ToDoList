3c4d536ade7f87110f1c5e24ae72a2aa77e033bd
Rafael Santos <chavesrafael9@gmail.com>
2023-11-05 01:26:10 -0300

    fix: remove properties from entity class

--- src/ToDoList.Domain/Entities/Entity.cs
+++ src/ToDoList.Domain/Entities/Entity.cs
@@ -5,15 +5,13 @@ namespace ToDoList.Domain.Entities
 
         public Guid Id { get; private set; }
         public string Name { get; private set; }
-        public DateTime CreateTime { get; private set; }
-        public DateTime ModifyTime { get; private set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime ModifyTime { get; set; }
 
         public Entity(string name)
         {
             Id = Guid.NewGuid();
             Name = name;
-            CreateTime = DateTime.UtcNow;
-            ModifyTime = DateTime.UtcNow;
         }
     }
 }
