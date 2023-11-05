5dcec0b9118c9b0fa8597ee7773595b2f0f0a6f1
Rafael Santos <chavesrafael9@gmail.com>
2023-11-05 01:46:41 -0300

    feat: create class AddInfrastructure in .IoC

---
+++ src/ToDoList.Infra/ToDoList.Infra.Ioc/DependencyInjectionAPI.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDoList.Infra.Ioc
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
