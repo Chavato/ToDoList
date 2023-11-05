03fee8297320352331cd13ca3c5df68dbeaff4ea
Rafael Santos <chavesrafael9@gmail.com>
2023-11-05 01:46:06 -0300

    fix: add classes to include in database in ApplicationDbContext class

--- src/ToDoList.Infra/ToDoList.Infra.Data/Context/ApplicationDbContext.cs
+++ src/ToDoList.Infra/ToDoList.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Entities;
 using ToDoList.Infra.Data.Identity;
 
 namespace ToDoList.Infra.Data.Context
@@ -14,5 +15,14 @@ namespace ToDoList.Infra.Data.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     => optionsBuilder
         .UseSnakeCaseNamingConvention();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Card> Card { get; set; }
+        public DbSet<CheckList> CheckList { get; set; }
+        public DbSet<Item> Item { get; set; }
     }
 }
