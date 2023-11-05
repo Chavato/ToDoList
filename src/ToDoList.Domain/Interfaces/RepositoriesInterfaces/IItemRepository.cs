4f338bc6a4568790d1a7672eca473247bc1a2af9
Rafael Santos <chavesrafael9@gmail.com>
2023-11-05 01:33:50 -0300

    fix: change interface name

--- src/ToDoList.Domain/Interfaces/RepositoriesInterfaces/IItemRepository.cs
+++ src/ToDoList.Domain/Interfaces/RepositoriesInterfaces/IItemRepository.cs
@@ -2,7 +2,7 @@ using ToDoList.Domain.Entities;
 
 namespace ToDoList.Domain.Interfaces.RepositoriesInterfaces
 {
-    public interface IItemInterface : IRepository<Item>
+    public interface IItemRepository : IRepository<Item>
     {
         
     }
