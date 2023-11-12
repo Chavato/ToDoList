f3e9bc2be8402978835825f891e2f5ff231a4044
Rafael Santos <chavesrafael9@gmail.com>
2023-11-11 22:50:34 -0300

    feat: implements infrastructure api method

--- src/ToDoList.Infra/ToDoList.Infra.Ioc/DependencyInjectionAPI.cs
+++ src/ToDoList.Infra/ToDoList.Infra.Ioc/DependencyInjectionAPI.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Domain.Interfaces.RepositoriesInterfaces;
+using ToDoList.Infra.Data.Context;
+using ToDoList.Infra.Data.Identity;
+using ToDoList.Infra.Data.Repositories;
 
 namespace ToDoList.Infra.Ioc
 {
@@ -8,8 +14,49 @@ namespace ToDoList.Infra.Ioc
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
 
-            throw new NotImplementedException();
+            services.ConfigureDatabase(configuration);
+            services.AddDependencyInjectionRepositories();
+            services.AddDependencyInjectionServices();
 
+            return services;
+        }
+
+        private static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("ConnectionString");
+            if (connectionString == null)
+            {
+                //TODO: Search a better exception to throw in this situation.
+                throw new Exception("Connection string is not setted.");
+            }
+
+            services.AddDbContext<ApplicationDbContext>(opt =>
+                                                            opt.UseSqlServer(connectionString,
+                                                                             b => b.MigrationsAssembly(typeof(ApplicationDbContext)
+                                                                                                            .Assembly
+                                                                                                            .FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+        private static IServiceCollection AddDependencyInjectionRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICardRepository, CardRepository>();
+            services.AddScoped<CheckListRepository, CheckListRepository>();
+            services.AddScoped<ItemRepository, ItemRepository>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddDependencyInjectionServices(this IServiceCollection services)
+        {
+            //TODO: Register dependency injection services here.
+            return services;
         }
 
     }
