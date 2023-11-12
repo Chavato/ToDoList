8c53d1d00f1e2d696ccf61562d7858948a902f9a
Rafael Santos <chavesrafael9@gmail.com>
2023-11-11 22:50:49 -0300

    feat: implement infrastructure jwt method

---
+++ src/ToDoList.Infra/ToDoList.Infra.Ioc/DependencyInjectionJWT.cs
@@ -0,0 +1,56 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ToDoList.Infra.Ioc
+{
+    public static class DependencyInjectionJWT
+    {
+        public static IServiceCollection AddInfrastructureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? validIssuerJWT = configuration["JWT_VALID_ISSUER"];
+            string? validAudienceJWT = configuration["JWT_VALID_ISSUER"];
+            string? secretJWT = configuration["JWT_VALID_ISSUER"];
+
+            if (validAudienceJWT.IsNullOrEmpty() || validIssuerJWT.IsNullOrEmpty() || secretJWT.IsNullOrEmpty())
+            {
+                //Create a better exception to this situation.
+                throw new Exception("JWT Environment Variables are not setted.");
+            }
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidIssuer = validIssuerJWT,
+                    ValidAudience = validAudienceJWT,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretJWT))
+                };
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 4;
+            });
+
+            return services;
+        }
+    }
+}
