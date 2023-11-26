fdfde5b449eb6fcce5d4beb2ef218c5d6ffb73cf
Rafael Santos <chavesrafael9@gmail.com>
2023-11-26 13:43:24 -0300

    fix: alter access of foreign key item

--- src/ToDoList.Domain/Entities/Item.cs
+++ src/ToDoList.Domain/Entities/Item.cs
@@ -3,13 +3,14 @@ namespace ToDoList.Domain.Entities
     public class Item : Entity
     {
         public bool IsDone { get; private set; }
-        public Guid CheckListId { get; set; }
+        public Guid CheckListId { get; private set; }
 
         public CheckList? CheckList { get; set; }
 
-        public Item(string name, bool isDone) : base(name)
+        public Item(string name, bool isDone, Guid checkListId) : base(name)
         {
             IsDone = isDone;
+            CheckListId = checkListId;
         }
 
         public void Update(string name, bool isDone)
