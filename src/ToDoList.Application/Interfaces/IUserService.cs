4e9e5f928d9b94ebc0c47783776f55577626c90b
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2024-02-26 13:49:34 -0300

    feat: create methods in IUserService

--- src/ToDoList.Application/Interfaces/IUserService.cs
+++ src/ToDoList.Application/Interfaces/IUserService.cs
@@ -1,7 +1,14 @@
+using ToDoList.Application.Models.DTOs;
+
 namespace ToDoList.Application.Interfaces
 {
     public interface IUserService
     {
-        
+        Task<bool> AuthenicateUser(string email, string password);
+        Task<ApplicationUserDto> RegisterUser(string email, string password, IEnumerable<string> roles);
+        Task<ApplicationUserDto> UpdateUser(ApplicationUserDto applicationUser);
+        Task DeleteUser(string userId);
+        Task<ApplicationUserDto> ChangePassword(ApplicationUserDto applicationUser, string newPassword);
+        Task Logout();
     }
 }
--- src/ToDoList.Application/Services/UserService.cs
+++ src/ToDoList.Application/Services/UserService.cs
@@ -1,4 +1,5 @@
 using ToDoList.Application.Interfaces;
+using ToDoList.Application.Models.DTOs;
 using ToDoList.Domain.Interfaces.RepositoriesInterfaces;
 
 namespace ToDoList.Application.Services
@@ -10,6 +11,36 @@ namespace ToDoList.Application.Services
         public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
+        }
+
+        public Task<bool> AuthenicateUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUserDto> ChangePassword(ApplicationUserDto applicationUser, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUserDto> RegisterUser(string email, string password, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUserDto> UpdateUser(ApplicationUserDto applicationUser)
+        {
+            throw new NotImplementedException();
         }
     }
 }
