be41962387268dca08b1a2ba23e021b0be03d779
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2024-02-26 12:35:13 -0300

    feat: create user repository

--- src/ToDoList.Domain/Interfaces/RepositoriesInterfaces/IUserRepository.cs
+++ src/ToDoList.Domain/Interfaces/RepositoriesInterfaces/IUserRepository.cs
@@ -2,6 +2,11 @@ namespace ToDoList.Domain.Interfaces.RepositoriesInterfaces
 {
     public interface IUserRepository
     {
-        
+        Task<bool> AuthenticateUser(string email, string password);
+        Task<string> RegisterUser(string email, string password, IEnumerable<string> roles);
+        Task UpdateUser(string id, string email);
+        Task DeleteUser(string userId);
+        Task ChangePassword(string id, string newPassword);
+        Task Logout();
     }
 }
--- src/ToDoList.Infra/ToDoList.Infra.Data/Repositories/UserRepository.cs
+++ src/ToDoList.Infra/ToDoList.Infra.Data/Repositories/UserRepository.cs
@@ -4,6 +4,34 @@ namespace ToDoList.Infra.Data.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        
+        public Task<bool> AuthenticateUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangePassword(string id, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RegisterUser(string email, string password, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUser(string id, string email)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
