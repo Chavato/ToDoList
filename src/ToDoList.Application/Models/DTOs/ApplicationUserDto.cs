f51efe8d787cc4c8d5f519b643a9e8f1455c929b
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2024-02-27 20:52:39 -0300

    chore: update apĺicationuser

--- src/ToDoList.Application/Models/DTOs/ApplicationUserDto.cs
+++ src/ToDoList.Application/Models/DTOs/ApplicationUserDto.cs
@@ -2,6 +2,9 @@ namespace ToDoList.Application.Models.DTOs
 {
     public class ApplicationUserDto
     {
-        
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
     }
 }
--- src/ToDoList.Infra/ToDoList.Infra.Data/Identity/ApplicationUser.cs
+++ src/ToDoList.Infra/ToDoList.Infra.Data/Identity/ApplicationUser.cs
@@ -4,6 +4,16 @@ namespace ToDoList.Infra.Data.Identity
 {
     public class ApplicationUser : IdentityUser
     {
+
+        public ApplicationUser()
+        {
+
+        }
         
+        public ApplicationUser(string userName, string email)
+        {
+            Email = email;
+            UserName = userName;
+        }
     }
 }
