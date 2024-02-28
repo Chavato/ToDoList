b16fbf46053732936dc77cfa5639cc61f2f5d5d7
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2024-02-28 13:18:23 -0300

    feat: add data annotations to models webapi

--- src/ToDoList.WebApi/Models/RegisterUser.cs
+++ src/ToDoList.WebApi/Models/RegisterUser.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToDoList.WebApi.Models
 {
     public class RegisterUser
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        [Required]
+        [MaxLength(64)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(10, MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
     }
 }
