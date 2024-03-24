ef4eca43da40b053d92c53f9d60ff00624a9dc37
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2024-03-23 21:06:02 -0300

    chore: separate login layout responsability

--- client/to-do-list-web/src/app/pages/login/login.component.ts
+++ client/to-do-list-web/src/app/pages/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [
+        Validators.minLength(4),
+        Validators.required,
+      ]),
+    });
+  }
 }
